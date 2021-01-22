024d3bb255dd7f3711baea5ed2ae63ade148c002
ekaterinaasf <59531760+ekaterinaasf@users.noreply.github.com>
2021-01-22 20:06:12 +0100

    Update card.py

---
+++ utils/card.py
@@ -0,0 +1,15 @@
+class Symbol:
+```python
+def add(number_one: int, number_two: int) -> int:
+    """
+    Function that will perform the add operation between two numbers (in params).
+
+    :param number_one: An int that will be added to the second parameter.
+    :param number_two: An int that will be added to the fist parameter.
+    :return: An int that is the result of the two params being added to each other.
+    """
+    result = number_one + number_two
+    return result
+```
+  color=''
+  icon=[]
