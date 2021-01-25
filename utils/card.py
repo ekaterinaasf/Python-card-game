d10cfeab17b45604cdc0280e1fc71fa8f4f99934
ekaterinaasf <59531760+ekaterinaasf@users.noreply.github.com>
2021-01-25 08:45:57 +0100

    Update card.py

--- utils/card.py
+++ utils/card.py
@@ -1,15 +1,28 @@
 class Symbol:
 ```python
-def add(number_one: int, number_two: int) -> int:
+def __init__(color: str, icon: int) -> str:
     """
-    Function that will perform the add operation between two numbers (in params).
+    Function that will initialize the class with 2 attributes.
 
-    :param number_one: An int that will be added to the second parameter.
-    :param number_two: An int that will be added to the fist parameter.
-    :return: An int that is the result of the two params being added to each other.
+    :param color: A string that can have 2 values: black and red.
+    :param icon: A single character out of this list `[♥, ♦, ♣, ♠]`.
+    :return: An string that is the result of the two params being added to each other.
     """
     result = number_one + number_two
     return result
 ```
-  color=''
-  icon=[]
+    def __init__(self, color, icon):
+        self.color=color
+        self.icon=icon
+
+
+class Card(Symbol):
+```python
+    This class s inherits from class `Symbol` and have an additional attribute
+    `value` which is a string (one of  `['A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K']`)
+```
+    def __init__(self, color, icon, value):
+        self.value=value
+        
+        
+    
