0829e131fd08222101e13e8ef1efd3d54e70e7ac
ekaterinaasf <59531760+ekaterinaasf@users.noreply.github.com>
2021-01-25 09:06:46 +0100

    Update player.py

--- utils/player.py
+++ utils/player.py
@@ -1,14 +1,14 @@
 import Card from card.py
 
 class Player:
-```python
-  A class `Player` that contains these attributes:
-    :param cards: A list of `Card`. In a real card game, this is equivalent to the cards that the player has in his hands.
-    :param number_of_cards: An int starting a 0.
-    :param turn_count: An int starting a 0.
-    :param history: A list of `Card` that will contain all the cards played by the player
-    :return: A
-```
+  ```python
+    A class `Player` that contains these attributes:
+      :param cards: A list of `Card`. In a real card game, this is equivalent to the cards that the player has in his hands.
+      :param number_of_cards: An int starting a 0.
+      :param turn_count: An int starting a 0.
+      :param history: A list of `Card` that will contain all the cards played by the player
+      :return: A
+  ```
 
   def __init__(self, cards, turn_count, number_of_cards, history):
     self.cards=cards
@@ -22,3 +22,20 @@ class Player:
     print(`{} {} played: {} {}`.format(PLAYER_NAME, TURN_COUNT, CARD_NUMBER, CARD_SYMBOL_ICON))
     
     return cards[-1]
+  
+class Desk:
+  ```python
+    A class Deck contains 1 attribute and 3 methods:
+      :param cards: a list of instances of `Card`
+      :method fill_deck(): fill cards with a complete card game (an instance of  'A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K' for each possible symbol [♥, ♦, ♣, ♠])*. Your deck should contain **52 cards at the end**.
+- A `shuffle()` method that will shuffle all the list of `cards`.
+
+- A `` method that will 
+- A `distribute()` that will take a list of `Player` as parameter and will distribute the cards evenly between all the players.
+
+  def __init__(self):
+    self.cards=[]
+    
+  def fill_deck():
+    self.cards.append()
+
